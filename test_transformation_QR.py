1252c235975bf8214fc4bf5b1a4913111cc7ad3b
tparpaite <tparpaite@gmail.com>
2017-03-28 05:06:24 +0200

    Ajout des tests pour transformation QR

--- test_transformation_QR.py
+++ test_transformation_QR.py
@@ -1,20 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from transformation_QR import *
+from bidiag import *
+
+N_TESTS = 10
+
+
+def merge_logbook(logbook_array):
+    logbook = { 'time_exec': None, 'iteration': None, 'convergence': [] }
+    logbook["time_exec"] = np.mean([log["time_exec"] for log in logbook_array])
+    n_iteration = np.amax([len(log["iteration"]) for log in logbook_array])
+    logbook["iteration"] = range(n_iteration)
+
+    # Mean for convergence field
+    for i in range(n_iteration):
+        s = 0
+        for log in logbook_array:
+            if (i < len(log["iteration"])):
+                s += log["convergence"][i]
+        mean = s / len(logbook_array)
+        logbook["convergence"].append(mean)
+
+    return logbook
+
 
 def test_bidiagonal_to_diagonal():
-    S_bidiag = np.matrix([[2, 4, 0, 0],
-                   [0, 1, 3, 0],
-                   [0, 0, 5, 2],
-                   [0, 0, 0, 1],
-                   [0, 0, 0, 0],
-                   [0, 0, 0, 0]])
+    logbook_array = [None] * N_TESTS
 
-    (U, S_diag, V) = bidiagonal_to_diagonal(S_bidiag)
+    for i in range(N_TESTS):
+        M = np.matrix(np.random.randint(1000, size=(50, 50)))
+        QLeft, S_bidiag, QRight = mat_bidiagonale(M)
 
-    if (compare_bidiagonal(U * S_diag * V, S_bidiag)):
-        print("U x S x V = BD est verifie")
-    else:
-        print("U x S x V = BD n'est pas verifie")
+        (U, S_diag, V, logbook_array[i]) = bidiagonal_to_diagonal(S_bidiag)
+
+        if (not(compare_bidiagonal(U * S_diag * V, S_bidiag))):
+            print("Erreur : U x S x V = BD n'est pas verifie")
+            exit(1)
+        
+    logbook_merge = merge_logbook(logbook_array)
+    logbooks_to_figure(logbook_merge)
 
 
-def test_convergence():
+# Dessine la courbe en fonction des donnees contenues dans un logbook
+# X : Nombre d'iterations
+# Y : Convergence (somme des termes externes a la diagonale)
+def create_curve(fig, logbook, color, label):
+    X = logbook['iteration']
+    Y = logbook['convergence']
+
+    return fig.plot(X, Y, linestyle="-", marker="o", color=color, label=label, markevery=100)
+
     
+def logbooks_to_figure(logbook_basic):
+    fig, axes = plt.subplots(figsize=(15,6))
+
+    # On ajuste les axes
+    axes.set_xlabel("Iteration")
+    axes.set_ylabel("Nombre d'elements extradiagonaux non nuls (convergence)")
+    axes.set_xlim([0, 2000])
+    
+    # Creation des courbes
+    curve_basic = create_curve(axes, logbook_basic, "b", "Algorithme basique")
+
+    # Affichage graphique
+    curves = curve_basic
+    labels = [c.get_label() for c in curves]
+    axes.legend(curves, labels, loc="best")
+
+    # Sauvegarde au format PDF
+    filename = "figure_convergence.pdf"
+    fig.savefig(filename)
+    fig.show()
+    print filename + " successfully generated"
--- transformation_QR.py
+++ transformation_QR.py
@@ -1,18 +1,33 @@
 import numpy as np
+import time
 
 EPSILON = 1e-10
-
 
 def sum_bidiagonal_sup(S):
     """ PARAMS : S une matrice bidiagonale
         RETURN : La somme des carres de la diagonale sup
-    Cette fonction est utilisee pour montrer la convergence de l'algorithme."""
+    Cette fonction est utilisee pour verifier la convergence de l'algorithme"""
     (n, m) = np.shape(S)
     r = min(n, m)
     
     s = 0
     for i in range(r-1):
         s += np.square(S[i,i+1])
+
+    return s
+
+
+def convergence_indicator(S):
+    """ PARAMS : S une matrice bidiagonale
+        RETURN : La somme termes extradiagonaux non nuls (a EPSILON pret)
+    Cette fonction est utilisee pour montrer la convergence de l'algorithme (logbook)"""
+    (n, m) = np.shape(S)
+    r = min(n, m)
+    
+    s = 0
+    for i in range(r-1):
+        if (abs(S[i,i+1]) >= EPSILON):
+            s += 1
 
     return s
 
@@ -52,21 +67,38 @@ def compare_bidiagonal(A, B):
 
 def bidiagonal_to_diagonal(S):
     """ PARAMS : S une matrice bidiagonale
-        RETURN : (U, S, V) qui correspondent a la decomposition SVD
-        La matrice S converge vers une matrice diagonale """
+        RETURN : (U, S, V, logbook) qui correspondent a la decomposition SVD
+        La matrice S converge vers une matrice diagonale
+        Le logbook permet de verifier cette convergence a chaque iteration """
+
+    d = time.clock()
+
+    logbook = { 'time_exec': None, 'iteration': [], 'convergence': [] }
+    
     (n, m) = np.shape(S)
     U = np.matrix(np.eye(n, n))
     V = np.matrix(np.eye(m, m))
 
+    ite = 0
+
     while (not(is_diagonal(S))):
-        (Q1, R1) = np.linalg.qr(np.transpose(S))
-        (Q2, R2) = np.linalg.qr(np.transpose(R1))
+        # On ajoute les informations courantes au logbook
+        logbook["iteration"].append(ite)
+        logbook["convergence"].append(convergence_indicator(S))
+
+        # Puis on applique l'algorithme
+        (Q1, R1) = np.linalg.qr(np.transpose(S), mode='complete')
+        (Q2, R2) = np.linalg.qr(np.transpose(R1), mode='complete')
         S = R2
         U = U * Q2
         V = np.transpose(Q1) * V
         ite += 1
 
-    return (U, S, V)
+    f = time.clock()
+
+    logbook["time_exec"] = f - d
+
+    return (U, S, V, logbook)
 
     
         
